ec477beef45298c28fccef9e8e23e70ae51ade50
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-05-16 23:01:03 +0500

    Refactor Product view and URL routing; register Product model in admin

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
-
-router = DefaultRouter()
-router.register(r'products', ProductViewSet)
+from .views import ProductView
 
 urlpatterns = [
-    path('products/', include(router.urls)),
+    path('products/', ProductView.as_view(), name='products'),
 ]
--- base/views.py
+++ base/views.py
@@ -8,6 +8,6 @@ from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
